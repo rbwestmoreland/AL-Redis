d083fcf8c2194bf93c47f9bfd3256f80c1b669bc
Nathan Davis <nathand@angieslist.com>
2013-02-05 16:31:21 -0500

    cut 1.1

--- AngiesList.Redis/Properties/AssemblyInfo.cs
+++ AngiesList.Redis/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyValueStore")]
+[assembly: AssemblyTitle("AngiesList.Redis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("KeyValueStore")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Angie's List")]
+[assembly: AssemblyProduct("AngiesList.Redis")]
+[assembly: AssemblyCopyright("Copyright © Angie's List 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1")]
